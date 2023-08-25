25bb62f9910dfffea090c048c251354de8102459
Peu Sen <peusen113@gmail.com>
2023-08-25 15:57:19 +0530

    changes

--- app/src/main/java/com/rkbapps/jobgenie/screens/HomeScreen.kt
+++ app/src/main/java/com/rkbapps/jobgenie/screens/HomeScreen.kt
@@ -1,15 +1,20 @@
 package com.rkbapps.jobgenie.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Card
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.rkbapps.jobgenie.model.User
 
 @Composable
@@ -30,4 +35,15 @@ fun HomeScreen() {
     }
 
 
+}
+@Composable
+fun JobItem(){
+    Column(modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Top, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+        Card() {
+
+        }
+    }
 }
